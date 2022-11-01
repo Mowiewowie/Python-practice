a575c21ee031c1ef739976b26859e0fe2c3421db
George <george.mou61@gmail.com>
2022-10-31 20:48:46 -0500

    before classes

--- program.py
+++ program.py
@@ -58,10 +58,48 @@ print(fruits[1])
 (green, yellow, red) = fruits #unpacking tuples
 
 try:
-    fruits[1] = "grapes"
+    fruits[1] = "grapes" #will fail
 except:
     print("tuples are immutible!")
 finally:
     convertedToList = list(fruits)
     convertedToList[1] = "grapes"
     print(convertedToList[1])
+    
+#dictionaries
+myDict = {
+    "name": "Joe",
+    "age": 25,
+    "favorite color": "green"
+}
+myDict["grade"] = "A+"
+myDict.pop("favorite color")
+print(myDict["name"])
+print(len(myDict))
+secondDict = myDict.copy()
+
+#if statements
+if (5 > 10):
+    print("five is bigger than ten")
+elif (5 < 10):
+    print("five is smaller than ten")
+else:
+    print("give is equal to ten")
+    
+if 15 > 10 : print("fifteen is bigger than ten, shorthand")
+
+if (15 > 10):
+    pass #pass means don't do anything
+
+#functions. Python enforces number of parameters, unless * is used which converts it into a tuple
+print("functions")
+def MyFunction(*interger):
+    print("inside MyFunction!")
+    print(interger)
+    
+MyFunction(4)
+MyFunction()
+
+#lamda, like anynonomous functions in javascript: (x) => (x) {code here using x}
+myAnswer = lambda x, y : x + y
+print(myAnswer(10, 5))
